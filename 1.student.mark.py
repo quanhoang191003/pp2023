a9f7d82b182b960582f22ff6d9cdbc22632c5d99
quanhoang191003 <quanhoang1910.gmail.com>
2023-02-22 08:53:21 -0600

    change name

---
+++ 1.student.mark.py
@@ -0,0 +1,11 @@
+def Information():
+    def Information():
+    x = int(input("Enter number of students : "))
+    y = int(input("ENter number of courses : "))
+    for i in range(x):
+        STUDENT_NAME = input("Enter student's name : ")
+        STUDENT_ID = input("Enter student ID : ")
+        DOB = input("Enter DOB dd/mm/yy : ")
+    for i in range(y):
+        COURSE_NAME = input("Enter course name : ")
+        COURSE_ID = input("ENter course ID : ")
